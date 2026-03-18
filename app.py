db49e960f64ba2d9f752599a779ea467a326ce0f
anyafedorova <a.fedorova@tbs-education.org>
2026-03-18 22:17:47 +0000

    chache_data updated

--- app.py
+++ app.py
@@ -10,3 +10,10 @@ df = pd.read_csv("trips_data_1000.csv")
 st.write("Preview Uploaded data")
 st.dataframe(df.head())
 
+@st.cache_data
+def load_data(): 
+    df = pd.read_csv("datasets/trips_data_1000.csv")
+    df['pickup_time'] = pd.to_datetime(df['pickup_time'])
+    return df
+
+
