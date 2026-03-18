7a61e296feff6e5cfe0f6965bacaf7c17a973c11
anyafedorova <a.fedorova@tbs-education.org>
2026-03-18 22:04:37 +0000

    trips_data updated

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ print("today is a sunny day")
 import streamlit as st
 import pandas as pd
 
-st.title("trips_data_1000.csv")
-upload_file = st.file_uploader("Upload a CVS file", type=["csv"])
+st.title("CSV File viewer")
 
-if upload_file:
-    df = pd.read_csv(uploaded_file)
-    st.write("### Preview of Uploaded Data: ")
-    st.dataframe(df.head())
+df = pd.read_csv("trips_data_1000.csv")
+st.write("Preview Uploaded data")
+st.dataframe(df.head())
 
-st.sidebar.header("This is sidebar section")
