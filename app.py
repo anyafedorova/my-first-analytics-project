d2368ed47e3947c96a100444c8af4af74b12ae84
anyafedorova <a.fedorova@tbs-education.org>
2026-03-18 21:28:32 +0000

    more changes

--- app.py
+++ app.py
@@ -1,3 +1,57 @@
 print("Hello from GitHub Codespaces")
 print("today is a sunny day")
 
+import streamlit as st
+import pandas as pd
+
+st.title("Analyzing Car Sharing Data with Streamlit")
+
+st.sidebar.header("This is sidebar section")
+
+import streamlit as st
+
+st.title("My Streamlit Web App")
+
+# Contents in a sidebar
+with st.sidebar:
+    st.header("This is a sidebar Section")
+    user_input = st.text_input("Enter your name")
+    option = st.selectbox("Choose an option", ["Choice A", "Choice B", "Choice C"])
+
+# Contents in the main window
+st.header("This is the main section")
+# Display user input
+st.write(f"Hello, {user_input}! You selected option {option}.")
+
+# Adding Contents in multiple columns
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("Column 1")
+    st.button("Click me!")
+    st.write("This is some text in column 1")
+
+with col2:
+    st.subheader("Column 2")
+
+    st.line_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+    # Expandable section
+with st.expander("See explanation"):
+    st.write("This is an expandable section with additional information.")
+
+    import pandas as pd
+import numpy as np
+
+st.title("Manipulate Streamlit Chart")
+
+# Generate random data
+bar_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.bar_chart(bar_data)
+
+# Generate random data for line chart
+line_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.line_chart(line_data)
+
+# Generate random data for scatter chart
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.scatter_chart(chart_data)
