7b515c8dfc54a0576221b38a0cebf03a495344c9
anyafedorova <a.fedorova@tbs-education.org>
2026-03-19 09:57:18 +0000

    df uploaded

--- pages/car_sharing_dashboard.py
+++ pages/car_sharing_dashboard.py
@@ -1,3 +1,11 @@
 import streamlit as st
 import pandas as pd
 
+@st.cache_data
+def load_data():
+    trips = pd.read_csv("datasets/trips.csv")
+    cars = pd.read_csv("datasets/cars.csv")
+    cities = pd.read_csv("datasets/cities.csv")
+    return trips, cars, cities
+
+trips, cars, cities = load_data()
